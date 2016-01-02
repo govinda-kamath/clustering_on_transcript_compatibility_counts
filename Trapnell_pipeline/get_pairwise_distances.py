a1d16395306ab164ed82e548cade978fd3883774
Jesse Min Zhang <jessez@stanford.edu>
2016-01-02 15:46:02 -0800

    Finishing trap(1)

--- Trapnell_pipeline/get_pairwise_distances.py
+++ Trapnell_pipeline/get_pairwise_distances.py
@@ -28,11 +28,11 @@ def jensen_shannon(p,q):
     q = np.transpose(q[q > 0])
     m = np.transpose(m[m > 0])
      
-    #if entropy(m)-0.5*entropy(q)-0.5*entropy(p) < 0:
-    #    print (entropy(m)-0.5*entropy(q)-0.5*entropy(p))
-    #    print ('m = '+ str(entropy(m)))
-    #    print ('q = '+ str(entropy(q)))
-    #    print ('p = '+ str(entropy(p)))
+    if entropy(m)-0.5*entropy(q)-0.5*entropy(p) < 0:
+        print (entropy(m)-0.5*entropy(q)-0.5*entropy(p))
+        print ('m = '+ str(entropy(m)))
+        print ('q = '+ str(entropy(q)))
+        print ('p = '+ str(entropy(p)))
     return np.sqrt(entropy(m)-0.5*entropy(q)-0.5*entropy(p))
 
 print (sys.argv[1])
@@ -40,16 +40,16 @@ print (sys.argv[2])
 num_jobs=int(sys.argv[3])
 
 with open(sys.argv[1], 'rb') as infile:
-        Y = pickle.load(infile)
-        print (np.shape(Y))
+        X = pickle.load(infile)
+        print (np.shape(X))
 
-X=Y
+Y=X[:350,:]
 
-num_rows=np.shape(X)[0]
+num_rows=np.shape(Y)[0]
 for ind in range(num_rows):
-    print(X[ind].sum(), ind)
+    print(Y[ind].sum(), ind)
 
-D = pairwise_distances(X,metric=jensen_shannon,n_jobs=num_jobs)
+D = pairwise_distances(Y,metric=jensen_shannon,n_jobs=45)
 #D=np.zeros((num_rows,num_rows))
 
 #for i in range(num_rows):
