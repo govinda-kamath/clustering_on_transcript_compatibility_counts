daec655a13adb94e45c2b9a94a660a9e32da0593
Jesse Min Zhang <jessez@stanford.edu>
2016-01-02 13:44:38 -0800

    removed previous files

--- Trapnell_pipeline/get_pairwise_distances.py
+++ Trapnell_pipeline/get_pairwise_distances.py
@@ -3,11 +3,17 @@ from scipy.stats import entropy
 import pickle
 import numpy as np
 import sys
+import multiprocessing as mp
+import itertools
 
+print len(sys.argv)
 if len(sys.argv)!=4:
     print ('usage is \n python get_pairwise_distances.py ip-file op-file num-processes')
-
-def jensen_shannon(p, q):
+    exit(1)
+#def jensen_shannon(pqtuple):
+#    p=pqtuple[0]
+#    q=pqtuple[1]
+def jensen_shannon(p,q):
     #    pshape=np.shape(p)
     #    qshape=np.shape(q)
     #    assert pshape[1]==1
@@ -22,11 +28,11 @@ def jensen_shannon(p, q):
     q = np.transpose(q[q > 0])
     m = np.transpose(m[m > 0])
      
-    if entropy(m)-0.5*entropy(q)-0.5*entropy(p) < 0:
-        print (entropy(m)-0.5*entropy(q)-0.5*entropy(p))
-        print ('m = '+ str(entropy(m)))
-        print ('q = '+ str(entropy(q)))
-        print ('p = '+ str(entropy(p)))
+    #if entropy(m)-0.5*entropy(q)-0.5*entropy(p) < 0:
+    #    print (entropy(m)-0.5*entropy(q)-0.5*entropy(p))
+    #    print ('m = '+ str(entropy(m)))
+    #    print ('q = '+ str(entropy(q)))
+    #    print ('p = '+ str(entropy(p)))
     return np.sqrt(entropy(m)-0.5*entropy(q)-0.5*entropy(p))
 
 print (sys.argv[1])
@@ -34,10 +40,23 @@ print (sys.argv[2])
 num_jobs=int(sys.argv[3])
 
 with open(sys.argv[1], 'rb') as infile:
-        X = pickle.load(infile)
-        print (np.shape(X))
+        Y = pickle.load(infile)
+        print (np.shape(Y))
+
+X=Y
+
+num_rows=np.shape(X)[0]
+for ind in range(num_rows):
+    print(X[ind].sum(), ind)
 
 D = pairwise_distances(X,metric=jensen_shannon,n_jobs=num_jobs)
+#D=np.zeros((num_rows,num_rows))
+
+#for i in range(num_rows):
+#    print(i)
+#    pool=mp.Pool(processes=num_jobs)
+#    pqtuple=itertools.product([X[i,:]], X)
+#    D[i,:]=pool.map(jensen_shannon,pqtuple)
 
 with open(sys.argv[2],'wb') as outfile:
     pickle.dump(D, outfile, pickle.HIGHEST_PROTOCOL)
--- get_pairwise_distances.py
+++ get_pairwise_distances.py
@@ -2,6 +2,7 @@ from sklearn.metrics.pairwise import pairwise_distances
 from scipy.stats import entropy
 import pickle
 import numpy as np
+import sys
 
 def shannon_jensen(p, q):
     #    pshape=np.shape(p)
@@ -11,21 +12,30 @@ def shannon_jensen(p, q):
     #    assert pshape[0]==qshape[0]
     #    assert min(p) >= 0
     #    assert min(q) >= 0
-    #    assert sum(p)<=1 + np.finfo(float).eps and sum(p) >= 1- np.finfo(float).eps
-    #    assert sum(q)<=1 + np.finfo(float).eps and sum(q) >= 1- np.finfo(float).eps
+    #assert sum(p)<=1 + np.finfo(float).eps and sum(p) >= 1- np.finfo(float).eps
+    #assert sum(q)<=1 + np.finfo(float).eps and sum(q) >= 1- np.finfo(float).eps
     m=0.5*p+0.5*q
     p = np.transpose(p[p > 0])
     q = np.transpose(q[q > 0])
     m = np.transpose(m[m > 0])
+     
+    if entropy(m)-0.5*entropy(q)-0.5*entropy(p) < 0:
+        print entropy(m)-0.5*entropy(q)-0.5*entropy(p)
+        print 'm = '+ str(entropy(m))
+        print 'q = '+ str(entropy(q))
+        print 'p = '+ str(entropy(p))
     return np.sqrt(entropy(m)-0.5*entropy(q)-0.5*entropy(p))
 
-with open('/data/SS_RNA_seq/Code/sparse_eq_class.dat', 'rb') as infile:
+print sys.argv[1]
+print sys.argv[2]
+
+with open(sys.argv[1], 'rb') as infile:
         X = pickle.load(infile)
         print np.shape(X)
 
-D = pairwise_distances(X,metric=shannon_jensen,n_jobs=64)
+D = pairwise_distances(X,metric='manhattan',n_jobs=64)
 
-with open('/data/SS_RNA_seq/Code/pairwise_jensen_shannon_divergence.dat','wb') as outfile:
+with open(sys.argv[2],'wb') as outfile:
     pickle.dump(D, outfile, pickle.HIGHEST_PROTOCOL)
 
 
