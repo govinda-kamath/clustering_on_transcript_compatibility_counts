85738a67e20916dc129d135251da8372876c3a2d
Jesse Min Zhang <jessez@stanford.edu>
2016-01-03 11:40:22 -0800

    Added clean reads

--- Zeisel_pipeline/Zeisel_wrapper.py
+++ Zeisel_pipeline/Zeisel_wrapper.py
@@ -26,4 +26,15 @@ if not SRA_dir:
     sys.exit(1)
 
 print('Extracting reads from SRAs...')
-os.system('python process_SRA.py -i '+SRA_dir+' -o ./reads_with_UMIs/ -n '+str(num_proc))
+#os.system('mkdir -p ./reads_with_UMIs/')
+#os.system('rm -f ./reads_with_UMIs/*')
+#os.system('python process_SRA.py -i '+SRA_dir+' -o ./reads_with_UMIs/ -n '+str(num_proc))
+
+print('Separating reads and UMIs...')
+os.system('mkdir -p ./reads_and_UMI_subsample100/')
+os.system('mkdir -p ./tmp_dir/')
+os.system('rm -f ./reads_and_UMI_subsample100/*')
+os.system('rm -f ./tmp_dir/*')
+os.system('python Clean_reads.py -i ./reads_with_UMIs/ -o ./reads_and_UMI_subsample100/ '+
+          '-t ./tmp_dir/ -n '+str(num_proc))
+os.system('rmdir ./tmp_dir')
