f5e27b9c795f68c41d0787910da4ffa501ca004c
Jesse Min Zhang <jessez@stanford.edu>
2016-01-06 08:47:23 -0800

    edits

--- modified-kallisto-source/kallisto_pseudo_single/build/src/CMakeFiles/kallisto.dir/cmake_clean.cmake
+++ modified-kallisto-source/kallisto_pseudo_single/build/src/CMakeFiles/kallisto.dir/cmake_clean.cmake
@@ -2,6 +2,8 @@ file(REMOVE_RECURSE
   "CMakeFiles/kallisto.dir/main.cpp.o"
   "kallisto.pdb"
   "kallisto"
+  "kallisto.pdb"
+  "CMakeFiles/CMakeRelink.dir/kallisto"
 )
 
 # Per-language clean rules from dependency scanning.
