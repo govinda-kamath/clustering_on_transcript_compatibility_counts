9ed3004230744e6be7f887c9970b6680d9318bac
govinda-kamath <govinda.kamath@gmail.com>
2016-01-14 16:58:58 -0800

    removed netbeans residues

--- Timing_pipeline/time_test.py
+++ Timing_pipeline/time_test.py
@@ -53,16 +53,16 @@ kallisto_index='./kallisto_index/'
 bowtie_index='./bowtie_index/'
 hisat_index='./hisat_index/'
 
-os.system('rm -rf '+test_SRA_dir)
-os.system('rm -rf '+test_read_dir)
-os.system('rm -rf '+test_kallisto_dir)
-os.system('rm -rf '+test_kallipso_dir)
-os.system('rm -rf '+test_bowtie1_dir)
-os.system('rm -rf '+test_hisat_dir)
-os.system('rm -rf '+test_wc_dir)
-os.system('rm -rf '+kallisto_index)
-os.system('rm -rf '+bowtie_index)
-os.system('rm -rf '+hisat_index)
+#os.system('rm -rf '+test_SRA_dir)
+#os.system('rm -rf '+test_read_dir)
+#os.system('rm -rf '+test_kallisto_dir)
+#os.system('rm -rf '+test_kallipso_dir)
+#os.system('rm -rf '+test_bowtie1_dir)
+#os.system('rm -rf '+test_hisat_dir)
+#os.system('rm -rf '+test_wc_dir)
+#os.system('rm -rf '+kallisto_index)
+#os.system('rm -rf '+bowtie_index)
+#os.system('rm -rf '+hisat_index)
 
 os.system('mkdir -p '+test_SRA_dir)
 os.system('mkdir -p '+test_read_dir)
@@ -80,29 +80,29 @@ base_cmd1="wget -O ./SRA/"
 base_cmd2="ftp://ftp-trace.ncbi.nlm.nih.gov/sra/sra-instant/reads/ByStudy/sra/SRP/SRP045/SRP045452/"
 for flnames in files_picked:
     cmd=base_cmd1+flnames+".sra "+base_cmd2+flnames+"/"+flnames+'.sra'
-    os.system(cmd)
+    #os.system(cmd)
     
 print('Converting SRA to fastq.gz files')
 for flname in files_picked:
     cmd='fastq-dump --gzip '+ './SRA/'+flname+'.sra' ' -O '+ test_read_dir
-    os.system(cmd)
+    #os.system(cmd)
 
 print('Building kallisto index...')
 kallisto_index_path=kallisto_index+'Zeisel_index.idx'
-os.system(kallipso_path+' index -i '+kallisto_index_path+' '+ref_transcriptome)
+#os.system(kallipso_path+' index -i '+kallisto_index_path+' '+ref_transcriptome)
 
 print('Building hisat index...')
 hisat_ip_paths=''
 with open('hisat_chr_path_list.txt','r') as f:
     hisat_ip_paths=f.readline()
 hisat_index_path=hisat_index+'Zeisel_index'
-os.system('hisat-build  --offrate 5 '+hisat_ip_paths+' '+hisat_index_path)
+#os.system('hisat-build  --offrate 5 '+hisat_ip_paths+' '+hisat_index_path)
 
 print('Getting bowtie indices...')
 bowtie_index_dir='./bowtie_index/'
 os.system('mkdir -p '+bowtie_index_dir)
 bowtie_index_path=bowtie_index_dir+'Zeisel_index.all'
-os.system('bowtie-build --offrate=5 '+ref_transcriptome+' '+bowtie_index_path)
+#os.system('bowtie-build --offrate=5 '+ref_transcriptome+' '+bowtie_index_path)
 
 
 print('Timing kallisto...')
@@ -120,10 +120,10 @@ def run_kallisto():
         #print cmd
         os.system(cmd)
 
-x=timeit.timeit(run_kallisto,number=1)
-op_file=test_kallisto_dir+'time.time'
-with open(op_file,'w') as f:
-    f.write(str(x))
+#x=timeit.timeit(run_kallisto,number=1)
+#op_file=test_kallisto_dir+'time.time'
+#with open(op_file,'w') as f:
+#    f.write(str(x))
     
 print('Timing hacked kallisto...')
 def make_kallipso_path_global():
@@ -153,10 +153,10 @@ def run_word_count():
     for flname in flnames:
         read_path=test_read_dir+flname
         os.system('wc '+read_path)
-x=timeit.timeit(run_word_count,number=1)
-op_file=test_wc_dir+'time.time'
-with open(op_file,'w') as f:
-    f.write(str(x))        
+#x=timeit.timeit(run_word_count,number=1)
+#op_file=test_wc_dir+'time.time'
+#with open(op_file,'w') as f:
+#    f.write(str(x))        
 
 print('Timing hisat...')
 def run_hisat():
@@ -171,17 +171,17 @@ def run_hisat():
         out_sam=test_hisat_dir+cellname+'.sam'
         out_bam=test_hisat_dir+cellname+'.bam'
         read_fl=test_read_dir+fls
-        cmd="""hisat -x"""+transcriptome_path+ """  -U """ +test_read_dir+fls+" -S "+out_sam
+        cmd="""hisat -p 1 -x"""+transcriptome_path+ """  -U """ +test_read_dir+fls+" -S "+out_sam
         #print cmd
         cmd1="samtools view -bS "+out_sam+" > "+out_bam
         #print cmd1
         os.system(cmd)
         os.system(cmd1)
         
-x=timeit.timeit(run_hisat,number=1)
-op_file=test_hisat_dir+'time.time'
-with open(op_file,'w') as f:
-    f.write(str(x))
+#x=timeit.timeit(run_hisat,number=1)
+#op_file=test_hisat_dir+'time.time'
+#with open(op_file,'w') as f:
+#    f.write(str(x))
 
 print('Timing bowtie...')
 def run_bowtie1():
@@ -193,13 +193,13 @@ def run_bowtie1():
     for flname in flnames:
         out = test_bowtie1_dir + flname.split('.')[0]
         os.system('mkdir -p ' + out)
-        BTcmd = 'gzip -dc '+test_read_dir+flname+' | bowtie -aS --offrate 1 '+index+' - | samtools view -Sb - > '+out+'/hits.bam'
+        BTcmd = 'gzip -dc '+test_read_dir+flname+' | bowtie  -p 1 -aS --offrate 1 '+index+' - | samtools view -Sb - > '+out+'/hits.bam'
         #print BTcmd
         os.system(BTcmd)
     
-x=timeit.timeit(run_bowtie1,number=1)
-op_file=test_bowtie1_dir+'time.time'
-with open(op_file,'w') as f:
-    f.write(str(x)) 
+#x=timeit.timeit(run_bowtie1,number=1)
+#op_file=test_bowtie1_dir+'time.time'
+#with open(op_file,'w') as f:
+#    f.write(str(x)) 
 
         
