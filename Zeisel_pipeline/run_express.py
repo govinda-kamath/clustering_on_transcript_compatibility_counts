15c7147a37f5add77352b6ef10a77d9637eee4d4
govinda-kamath <govinda.kamath@gmail.com>
2016-01-14 16:58:58 -0800

    removed netbeans residues

--- Zeisel_pipeline/run_express.py
+++ Zeisel_pipeline/run_express.py
@@ -33,6 +33,7 @@ def run_express(fltuple):
     Ecmd = 'express -o '+hits+' ' + transcripts + ' ' + hits +'hits.bam'
     os.system(Ecmd)
 
+
 for suffix in ['100','10','5','1','_point5','_point1']:
     bam_dir='./Zeisel_Bowtie_subsample'+suffix+'/'
     flnames=os.listdir(bam_dir)
