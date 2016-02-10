5d7be026e40dfc0bfd5a53186e93af6002781a8f
govinda-kamath <govinda.kamath@gmail.com>
2016-02-10 14:09:46 -0800

    Timing reviewer comment handled by setting seed

--- Timing_pipeline/time_test.py
+++ Timing_pipeline/time_test.py
@@ -11,26 +11,23 @@ import getopt
 file_list='./SRR_in_3005.txt'
 
 cell_files=sorted(np.loadtxt(file_list,dtype=str))
-files_picked=np.random.choice(cell_files,10,replace=False)
-files_used_in_paper=['SRR1545085',  'SRR1546711', 'SRR1547170',
-                     'SRR1547184',  'SRR1547881', 'SRR1545232',
-                     'SRR1547119',  'SRR1547175', 'SRR1547531',  
-                     'SRR1548035']
+
 
 try:
-    opts, args = getopt.getopt(sys.argv[1:],"k:pr:h:",["hacked-kallisto-path","as-in-paper=","reference-transcriptome","mouse-genome-for-hisat"])
+    opts, args = getopt.getopt(sys.argv[1:],"k:s:r:h:",["hacked-kallisto-path","seed=","reference-transcriptome","mouse-genome-for-hisat"])
 except getopt.GetoptError:
     print ("getopterrror")
-    print ('usage is : \n python time_test.py -k hacked-kallisto-path -r mouse-reference-transcriptome -h path-to-file-containing-paths-to-mouse-genome [-p] \n -p indicates to use the same files as used in the simulation of the paper')
+    print ('usage is : \n python time_test.py -k hacked-kallisto-path -r mouse-reference-transcriptome -h path-to-file-containing-paths-to-mouse-genome [-s seed]')
     sys.exit(1)
 
 kallipso_path=''
 ref_transcriptome=''
 ref_genome=''
+npseed=100
 
 for opt,arg in opts:
-    if opt in ("-p", "--as-in-paper"):
-        files_picked=files_used_in_paper
+    if opt in ("-s", "--seed"):
+        npseed=int(arg)
     elif opt in ("-k","--hacked-kallisto-path"):
         kallipso_path=arg
     elif opt in ("-r","--reference-transcriptome"):
@@ -39,7 +36,7 @@ for opt,arg in opts:
         ref_genome=arg
 
 if (not kallipso_path) or (not ref_transcriptome) or (not ref_genome):
-    print ('usage is : \n python time_test.py -k hacked-kallisto-path -r mouse-reference-transcriptome -h path-to-file-containing-paths-to-mouse-genome [-p] \n -p indicates to use the same files as used in the simulation of the paper')
+    print ('usage is : \n python time_test.py -k hacked-kallisto-path -r mouse-reference-transcriptome -h path-to-file-containing-paths-to-mouse-genome [-s seed]')
     sys.exit(1)
 
 test_SRA_dir='./SRA/'
@@ -53,16 +50,16 @@ kallisto_index='./kallisto_index/'
 bowtie_index='./bowtie_index/'
 hisat_index='./hisat_index/'
 
-#os.system('rm -rf '+test_SRA_dir)
-#os.system('rm -rf '+test_read_dir)
-#os.system('rm -rf '+test_kallisto_dir)
-#os.system('rm -rf '+test_kallipso_dir)
-#os.system('rm -rf '+test_bowtie1_dir)
-#os.system('rm -rf '+test_hisat_dir)
-#os.system('rm -rf '+test_wc_dir)
-#os.system('rm -rf '+kallisto_index)
-#os.system('rm -rf '+bowtie_index)
-#os.system('rm -rf '+hisat_index)
+os.system('rm -rf '+test_SRA_dir)
+os.system('rm -rf '+test_read_dir)
+os.system('rm -rf '+test_kallisto_dir)
+os.system('rm -rf '+test_kallipso_dir)
+os.system('rm -rf '+test_bowtie1_dir)
+os.system('rm -rf '+test_hisat_dir)
+os.system('rm -rf '+test_wc_dir)
+os.system('rm -rf '+kallisto_index)
+os.system('rm -rf '+bowtie_index)
+os.system('rm -rf '+hisat_index)
 
 os.system('mkdir -p '+test_SRA_dir)
 os.system('mkdir -p '+test_read_dir)
@@ -75,34 +72,38 @@ os.system('mkdir -p '+kallisto_index)
 os.system('mkdir -p '+bowtie_index)
 os.system('mkdir -p '+hisat_index)
 
+np.random.seed(seed=100)
+files_picked=np.random.choice(cell_files,10,replace=False)
+
+
 print('Copying over SRA files...')
 base_cmd1="wget -O ./SRA/"
 base_cmd2="ftp://ftp-trace.ncbi.nlm.nih.gov/sra/sra-instant/reads/ByStudy/sra/SRP/SRP045/SRP045452/"
 for flnames in files_picked:
     cmd=base_cmd1+flnames+".sra "+base_cmd2+flnames+"/"+flnames+'.sra'
-    #os.system(cmd)
+    os.system(cmd)
     
 print('Converting SRA to fastq.gz files')
 for flname in files_picked:
     cmd='fastq-dump --gzip '+ './SRA/'+flname+'.sra' ' -O '+ test_read_dir
-    #os.system(cmd)
+    os.system(cmd)
 
 print('Building kallisto index...')
 kallisto_index_path=kallisto_index+'Zeisel_index.idx'
-#os.system(kallipso_path+' index -i '+kallisto_index_path+' '+ref_transcriptome)
+os.system(kallipso_path+' index -i '+kallisto_index_path+' '+ref_transcriptome)
 
 print('Building hisat index...')
 hisat_ip_paths=''
 with open('hisat_chr_path_list.txt','r') as f:
     hisat_ip_paths=f.readline()
 hisat_index_path=hisat_index+'Zeisel_index'
-#os.system('hisat-build  --offrate 5 '+hisat_ip_paths+' '+hisat_index_path)
+os.system('hisat-build  --offrate 5 '+hisat_ip_paths+' '+hisat_index_path)
 
 print('Getting bowtie indices...')
 bowtie_index_dir='./bowtie_index/'
 os.system('mkdir -p '+bowtie_index_dir)
 bowtie_index_path=bowtie_index_dir+'Zeisel_index.all'
-#os.system('bowtie-build --offrate=5 '+ref_transcriptome+' '+bowtie_index_path)
+os.system('bowtie-build --offrate=5 '+ref_transcriptome+' '+bowtie_index_path)
 
 
 print('Timing kallisto...')
@@ -120,10 +121,10 @@ def run_kallisto():
         #print cmd
         os.system(cmd)
 
-#x=timeit.timeit(run_kallisto,number=1)
-#op_file=test_kallisto_dir+'time.time'
-#with open(op_file,'w') as f:
-#    f.write(str(x))
+x=timeit.timeit(run_kallisto,number=1)
+op_file=test_kallisto_dir+'time.time'
+with open(op_file,'w') as f:
+    f.write(str(x))
     
 print('Timing hacked kallisto...')
 def make_kallipso_path_global():
@@ -153,10 +154,10 @@ def run_word_count():
     for flname in flnames:
         read_path=test_read_dir+flname
         os.system('wc '+read_path)
-#x=timeit.timeit(run_word_count,number=1)
-#op_file=test_wc_dir+'time.time'
-#with open(op_file,'w') as f:
-#    f.write(str(x))        
+x=timeit.timeit(run_word_count,number=1)
+op_file=test_wc_dir+'time.time'
+with open(op_file,'w') as f:
+    f.write(str(x))        
 
 print('Timing hisat...')
 def run_hisat():
@@ -178,10 +179,10 @@ def run_hisat():
         os.system(cmd)
         os.system(cmd1)
         
-#x=timeit.timeit(run_hisat,number=1)
-#op_file=test_hisat_dir+'time.time'
-#with open(op_file,'w') as f:
-#    f.write(str(x))
+x=timeit.timeit(run_hisat,number=1)
+op_file=test_hisat_dir+'time.time'
+with open(op_file,'w') as f:
+    f.write(str(x))
 
 print('Timing bowtie...')
 def run_bowtie1():
@@ -197,9 +198,9 @@ def run_bowtie1():
         #print BTcmd
         os.system(BTcmd)
     
-#x=timeit.timeit(run_bowtie1,number=1)
-#op_file=test_bowtie1_dir+'time.time'
-#with open(op_file,'w') as f:
-#    f.write(str(x)) 
+x=timeit.timeit(run_bowtie1,number=1)
+op_file=test_bowtie1_dir+'time.time'
+with open(op_file,'w') as f:
+    f.write(str(x)) 
 
         
